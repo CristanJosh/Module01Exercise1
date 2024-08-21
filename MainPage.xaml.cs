6616900ee0bc3032b6bd38dc5af14b073a718f32
CristanJosh <cristanjosh03@gmail.com>
2024-08-21 13:07:32 +0800

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace Module01Activity02
+{
+    public partial class MainPage : ContentPage
+    {
+       
+        public MainPage()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void OnSaveChangesClicked(object sender, EventArgs e)
+        {
+
+            ConfirmationMessage.IsVisible = true; // confirmation message
+        }
+
+        private void OnChangedBgColor(object sender, EventArgs e)
+        {
+            this.Resources["DynamicBackgroundColor"] = Colors.Pink;
+        }
+    }
+}
